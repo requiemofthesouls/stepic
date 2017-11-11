c2cd7a8c913e5dc9c3ebba55e268b75627d5e731
requiemofthesouls <dimamid99@gmail.com>
2017-11-11 21:43:20 +0500

    изменил конфиг

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,3 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo cp -f /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
+  sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+  sudo rm -rf /etc/nginx/sites-enabled/default
+  sudo /etc/init.d/nginx restart
