6d118673c9293b0ed63e1fdb6c5c7a467e515f93
Konstantin <32811996+requiemofthesouls@users.noreply.github.com>
2017-11-12 13:33:33 +0500

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -1,2 +1,9 @@
-  sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-  sudo /etc/init.d/nginx restart
+sudo unlink /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+
+
+ # sudo /etc/init.d/nginx restart
+ # ps -o pid,euser,egroup,comm,args -C nginx
+
+ # sudo nginx -c /usr/local/etc/nginx/nginx.conf -t
